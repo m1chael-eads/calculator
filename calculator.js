e96a03b6de14d3a58df74d27f2f09f1b189c2b95
m1chael-eads <186185265+m1chael-eads@users.noreply.github.com>
2025-03-15 18:27:27 -0400

    Refactor logic for calculate() and appendToDisplay()

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ number1 = 0;
 number2 = 0;
 operator = "";
 input = "";
+termList = [];
 
 function add(num1,num2) {
     return num1 + num2;
@@ -18,7 +19,8 @@ function multiply(num1,num2) {
 }
 
 function divide(numerator,denom) {
-    return (numerator/denom).toFixed(2);
+    if (denom == 0) return "nice try!"; 
+    else return (numerator/denom).toFixed(2);
 }
 
 function operate(op,num1,num2) {
@@ -45,29 +47,35 @@ function clearDisplay() {
 }
 
 function calculate() {
-    array = input.split(" ");
-    number1 = array[0];
-    operator = array[1];
-    number2 = array[2];
+    number1 = termList[0];
+    operator = termList[1];
+    number2 = termList[2];
+    if (termList.length == 3) {
+        input = operate(operator,number1,number2);
+        termList = [];
+        termList += input;
+    }
+    else {
+        input = `${operate(operator,number1,number2)}` + ` ` +`${termList[3]} `
+        termList = input.split(" ");
+    }
     resText.data = operate(operator,number1,number2);
-    input = `${operate(operator,number1,number2)} ` + `${array[3]} `;
 }
 
 function appendToDisplay(button) {
     value = button.textContent;
-    if (isOperation(value)) {
+    if (isOperation(value)) {  
         input += " " + value + " ";
-        if (input.split(" ").length > 3) {
-            calculate();
-        }
     }
     else {
-        if (input.charAt(input.length-1) == " ") {
+        if (input.charAt(input.length - 1) == " ") {
             resText.data = "";
         }
-        input += value;
         resText.data += value;
+        input += value;
     }
+    termList = input.trimEnd().split(" ").filter(element => element);
+    if (termList.length > 3) {calculate();}
 }
 
 function isOperation(string) {
@@ -77,6 +85,19 @@ function isOperation(string) {
     else return false;
 }
 
+function isSimplifyable(input) {
+    console.log("isSimplifyable() runs");
+    array = [];
+    for (char in input) {
+        if (isOperation(input[char])) {
+            array = input.split(input[char]);
+        }
+    }
+    console.log("array: " + array);
+    if (array.length >= 2) {return true;}
+    else {return false;}
+}
+
 
 
 const result = document.getElementById('result');
@@ -84,3 +105,5 @@ result.classList.add('result');
 resText = document.createTextNode('');
 result.appendChild(resText);
 
+/* Write a function called appendToDisplay(button) that stores the value of the clicked button and does the following. If the button is a number, append that number to the display and a string variable input. 
+If the button is an operator, append the operator to the string variable input but not to the display and check if input needs to be simplified. If the button is =, evaluate the current expression stored in input.*/
